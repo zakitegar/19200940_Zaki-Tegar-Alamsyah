0471c7ef4ac5eba8d684eca5d1a68d49e4e74e37
19200940_ZakiTegar <90232788+zakitegar@users.noreply.github.com>
2021-10-07 00:08:03 +0700

    upload tugas pertemuan 4
    upload tugas pada pertemuan 4

---
+++ v_about.php
@@ -0,0 +1,21 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Zaki Tegar</li> 
+        <li>Nama Belakang : Alamsyah</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Jalan Ciledug Raya No. 168 Pesanggrahan</li> 
+    </ul> 
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Jakarta</li> </ul> 
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Renang</li> 
+        <li>Futsal</li> 
+    </ul> 
+</section>
